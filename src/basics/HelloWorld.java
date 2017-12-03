f6684e5524ce84a6d35626ccf1ef92e6fe98d3cb
phandb <phandb@users.noreply.github.com>
2017-12-03 00:14:09 -0600

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I am gonna add codes to my project");
+		System.out.println("I am gonna add codes from GitHub");
 	}
 	
 
